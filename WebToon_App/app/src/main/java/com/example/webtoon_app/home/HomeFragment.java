258daac724efc060efd942b3f7e9a74eb48deae9
MINJU KIM <yd5726@naver.com>
2022-12-12 19:29:25 +0900

    Update WebToon_App
    Update WebToon_App

--- WebToon_App/app/src/main/java/com/example/webtoon_app/home/HomeFragment.java
+++ WebToon_App/app/src/main/java/com/example/webtoon_app/home/HomeFragment.java
@@ -8,24 +8,30 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 import com.example.webtoon_app.R;
 import com.example.webtoon_app.webtoon.WT_Detail_Activity;
 
 public class HomeFragment extends Fragment {
-    ImageView banner1;
+    Animation slide_up_enter;
+    ImageView WT3H_1;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_home, container, false);
-        banner1 = v.findViewById(R.id.banner1);
-        banner1.setOnClickListener(new View.OnClickListener() {
+
+        slide_up_enter = AnimationUtils.loadAnimation(getContext(),R.anim.slide_up_enter);
+        WT3H_1 = v.findViewById(R.id.WT3H_1);
+        WT3H_1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getContext(), WT_Detail_Activity.class);
                 startActivity(intent);
+                WT3H_1.startAnimation(slide_up_enter);
             }
         });
 
--- WebToon_App/app/src/main/java/com/example/webtoon_app/webtoon/WT_Detail_Activity.java
+++ WebToon_App/app/src/main/java/com/example/webtoon_app/webtoon/WT_Detail_Activity.java
@@ -1,16 +1,33 @@
 package com.example.webtoon_app.webtoon;
 
+import static java.security.AccessController.getContext;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
+import com.example.webtoon_app.MainActivity;
 import com.example.webtoon_app.R;
+import com.example.webtoon_app.home.HomeFragment;
 
 public class WT_Detail_Activity extends AppCompatActivity {
+    ImageView iv_back_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_webtoon_detail);
+
+        iv_back_btn = findViewById(R.id.iv_back_btn);
+        iv_back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
