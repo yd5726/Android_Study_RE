dc12db8c6ad4ce1b9825e387b995a1181c2747eb
MINJU KIM <yd5726@naver.com>
2022-12-14 19:07:27 +0900

    Update WebToon_App : 웹툰 틀
    Update WebToon_App : 웹툰 틀

--- WebToon_App/app/src/main/java/com/example/webtoon_app/MainActivity.java
+++ WebToon_App/app/src/main/java/com/example/webtoon_app/MainActivity.java
@@ -5,9 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.webtoon_app.home.HomeFragment;
@@ -58,9 +64,27 @@ public class MainActivity extends AppCompatActivity {
         if(System.currentTimeMillis() > pressedTime + 2000){
             pressedTime = System.currentTimeMillis();
             Toast.makeText(getApplicationContext(), "'뒤로'한번 더 누르시면 종료됩니다.", Toast.LENGTH_SHORT).show();
+            //View v = null;
+            //writeCustomToast(v);
         }else{
             ActivityCompat.finishAffinity(this);
             System.exit(0);
         }
     }
+    public void writeCustomToast(View view) {
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(
+                R.layout.custom_toast_layout,
+                findViewById(R.id.custom_toast_container));
+        TextView text = layout.findViewById(R.id.tv_toast);
+        Toast toast = new Toast(this);
+        text.setText("토스트 띄우기");
+        text.setTextSize(15);
+        text.setTextColor(Color.BLACK);
+        toast.setGravity(Gravity.BOTTOM,0,0);
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setView(layout);
+        toast.show();
+    }
+
 }
--- WebToon_App/app/src/main/java/com/example/webtoon_app/webtoon/WebtoonFragment.java
+++ WebToon_App/app/src/main/java/com/example/webtoon_app/webtoon/WebtoonFragment.java
@@ -2,6 +2,7 @@ package com.example.webtoon_app.webtoon;
 
 import android.os.Bundle;
 
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -11,11 +12,15 @@ import android.view.ViewGroup;
 import com.example.webtoon_app.R;
 
 public class WebtoonFragment extends Fragment {
+    Toolbar toolbar;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_webtoon, container, false);
+        toolbar = v.findViewById(R.id.toolbar);
+        toolbar.setTitle("웹툰");
+
         return v;
     }
 }
